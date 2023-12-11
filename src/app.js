9455993c3d8885530466101203522b9e322c1203
Rubel Parvaz <rubelr1818@gmail.com>
2023-12-11 11:04:16 +0530

    express js rubel

---
+++ src/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var hbs = require('hbs');
+var app = express();
+var staticpath = path.join(__dirname, '../public');
+var templatespath = path.join(__dirname, '../templates/views');
+var partialspath = path.join(__dirname, '../templates/partials');
+app.use(express.static(staticpath));
+app.set('view engine', 'hbs')
+app.set('views', templatespath)
+hbs.registerPartials(partialspath)
+app.get('/', (req, res) => {
+  const data = {
+    title: 'Home page',
+    message: 'Hello, Handlebars!',
+    first_name: 'Rubel',
+    last_name: 'Parvaz',
+  };
+
+  res.render('index', data);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
