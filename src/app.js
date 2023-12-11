a5aecc384369dc83e1d88d9793ea6e2fdbb4e37f
Rubel Parvaz <rubelr1818@gmail.com>
2023-12-11 12:44:03 +0530

    Message

--- src/app.js
+++ src/app.js
@@ -20,6 +20,11 @@ app.get('/', (req, res) => {
   res.render('index', data);
 });
 
+app.get('*', (req, res) => {
+  res.render('404');
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
